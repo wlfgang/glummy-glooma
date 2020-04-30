9e3c6ec6d249b121777d2671619ec0905a308c89
wlfgang <14792753+wlfgang@users.noreply.github.com>
2020-04-30 14:22:03 -0400

    Update ffmpeg, attempt to read all frames

--- main.cpp
+++ main.cpp
@@ -15,11 +15,24 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    auto numFrames = static_cast<size_t>(videoCapture.get(cv::CAP_PROP_FRAME_COUNT));
+    std::cout << "Number of frames: " << numFrames << std::endl;
+
     // grab the first frame and write it out to show it's working
     cv::Mat image;
     videoCapture >> image;
     std::cout << "Image dimensions: " << image.cols << "x" << image.rows << std::endl;
     cv::imwrite("first_frame.png", image);
+
+    for (size_t i = 1; i < numFrames; i++) {
+        auto frame = static_cast<size_t>(videoCapture.get(cv::CAP_PROP_POS_FRAMES));
+        std::cout << "Reading frame " << frame << "..." << std::endl;
+
+        if (!videoCapture.read(image) || image.empty()) {
+            std::cout << "Error reading frame " << frame << std::endl;
+        }
+    }
+
     videoCapture.release();
 
     return 0;
